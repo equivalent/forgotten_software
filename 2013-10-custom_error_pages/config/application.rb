65db0d6a01d0cbb6ae413bd95e46f660405617cf
Tomi <equivalent@eq8.eu>
2013-10-07 11:55:35 +0100

    Update application.rb

--- 2013-10-custom_error_pages/config/application.rb
+++ 2013-10-custom_error_pages/config/application.rb
@@ -1,8 +1,8 @@
-
 module MyApp
   class Application < Rails::Application
     # ...
-    config.exceptions_app = self.routes 
+    config.exceptions_app = self.routes  # this will tell Rack to use own app (own controllers)
+                                         # to route errors
     # ..
   end
 end
